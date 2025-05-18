a0ccff90b95408be884b1fbc289f53f66c0d1727
tarekkkk-b <tarekbadawi2004@gmail.com>
2025-05-18 16:51:34 +0400

    saving current work

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -1,26 +1,50 @@
 #pragma once
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "Channel.hpp"
+#include "Server.hpp"
 
 class Client
 {
 	private:
-	// std::string hostname;                // Host of the client (optional)
-	// std::string ipAddress;     //mand         // IP address (optional, useful for info)
-	std::string nickname;      //on start         // Set via NICK command
-	std::string username;       // on start        // Set via USER command
-	std::string realname;       // on start        // From USER command (after :)
-	std::string password;       // on start        // From PASS command
-	bool authenticated;    //once all is recieved       // True only if PASS + NICK + USER are all sent
-	bool registered;       // optional             // True after full registration (can join channels)
-	// bool operatorMode;                  // Whether the user is an operator (for channels)
-	
-	// std::string buffer;                 // For accumulating partial messages
-	// std::vector<std::string> channels;  // Names of channels this user is part of
-	
+		int socketFd;       	//mand                 // Unique socket for communication
+    	std::string nickname;	//on start         // Set via NICK command
+    	std::string username;	// on start        // Set via USER command
+    	std::string realname;	// on start        // From USER command (after :)
+    	std::string hostname;	// on start        // From USER command (after :)
+    	std::string password;	// on start        // From PASS command
+    	bool authenticated;		//once all is recieved       // True only if PASS + NICK + USER are all sent
+    	// bool registered;       // optional             // True after full registration (can join channels)
+    	// bool operatorMode;                  // Whether the user is an operator (for channels)
+
+    	// std::string buffer;                 // For accumulating partial messages
+    	std::vector<std::string> channels;  // Names of channels this user is part of
+
 	public:
-		int socketFd;       //mand                 // Unique socket for communication
+		Client();
 		Client(int _socketFd);
 		Client(const Client &copy);
 		Client &operator=(const Client &ref);
 		~Client();
+
+		bool getAuth() const;
+		std::string getUser() const;
+		std::string getNick() const;
+		std::string getName() const;
+		std::string getPass() const;
+		
+		void setAuth(bool status);
+		void setUser(const std::string _username);
+		void setNick(const std::string _nickname);
+		void setName(const std::string _name);
+		void setPass(const std::string _pass);
+
+		// void joinChannel(const std::string _channelName) const;
+		void joinChannel(const std::vector<std::string> args, Server &server) const;
+		void leaveChannel(const std::string _channelName) const;
+
+		void parseUserCommands(const std::string msg);
 };
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,10 +1,19 @@
 #include "../inc/Client.hpp"
 
+Client::Client()
+{
+	this->socketFd = -1;
+	this->authenticated = false;
+	this->realname = "";
+	this->username = "";
+	this->nickname = "";
+	this->password = "";
+}
+
 Client::Client(int _socketFd)
 {
 	this->socketFd = _socketFd;
 	this->authenticated = false;
-	this->registered = false;
 	this->realname = "";
 	this->username = "";
 	this->nickname = "";
@@ -13,12 +22,7 @@ Client::Client(int _socketFd)
 
 Client::Client(const Client &copy)
 {
-	this->nickname = copy.nickname;
-	this->username = copy.username;
-	this->realname = copy.realname;
-	this->password = copy.password;
-	this->authenticated = copy.authenticated;
-	this->registered = copy.registered;
+	*this = copy;
 }
 
 Client &Client::operator=(const Client &ref)
@@ -28,10 +32,135 @@ Client &Client::operator=(const Client &ref)
 	this->realname = ref.realname;
 	this->password = ref.password;
 	this->authenticated = ref.authenticated;
-	this->registered = ref.registered;
 	return *this;
 }
 
 Client::~Client()
 {
+}
+
+bool Client::getAuth() const
+{
+	return this->authenticated;
+}
+
+std::string Client::getUser() const
+{
+	return this->username;
+}
+
+std::string Client::getNick() const
+{
+	return this->nickname;
+}
+
+std::string Client::getName() const
+{
+	return this->realname;
+}
+
+std::string Client::getPass() const
+{
+	return this->password;
+}
+void Client::setAuth(bool status)
+{
+	this->authenticated = status;
+}
+
+void Client::setUser(const std::string _username)
+{
+	this->username = _username;
+}
+
+void Client::setNick(const std::string _nickname)
+{
+	this->nickname = _nickname;
+}
+
+void Client::setName(const std::string _name)
+{
+	this->realname = _name;
+}
+
+void Client::setPass(const std::string _pass)
+{
+	this->password = _pass;
+}
+
+void	Client::joinChannel(const std::vector<std::string> args, Server &server) const
+{
+	if (args.size() == 3)
+	{
+
+	}
+}
+
+// void Client::joinChannel(const std::string _channelName) const
+// {
+// 	(void)_channelName;
+// }
+
+void Client::leaveChannel(const std::string _channelName) const
+{
+	(void)_channelName;
+}
+
+static std::vector<std::string> splitWords(const std::string msg)
+{
+	std::istringstream iss(msg);
+	std::vector<std::string> words;
+	std::string temp;
+
+	while (iss >> temp)
+		words.push_back(temp);
+	return words;
+}
+
+void Client::parseUserCommands(const std::string msg)
+{
+	if (msg.empty())
+	{
+		std::cerr << "empty message" << std::endl;
+		return ;
+	}
+	std::vector<std::string> words = splitWords(msg);
+	std::string commands[5] = {"/NICK", "/PASS", "/JOIN", "/NAME", "/USER"};
+	int i = 0;
+	while (i < 5)
+	{
+		if (words[0] == commands[i])
+			break ;
+		i++;
+	}
+	if (words.size() != 2)
+		return ;
+	switch (i)
+	{
+		case  0:
+			this->setNick(words[1]);
+			break;
+		case  1:
+			this->setPass(words[1]);
+			break;
+		// case  2:
+		// 	this->joinChannel(words[1]);
+		// 	break;
+		case  3:
+			this->setName(words[1]);
+			break;
+		case  4:
+			this->setUser(words[1]);
+			break;
+	}
+}
+
+
+
+
+void check_commands(const std::string msg)
+{
+	std::vector<std::string> arguments = splitWords(msg);
+	std::string commands[5] = {"/NICK", "/PASS", "/JOIN", "/NAME", "/USER"};
+	
 }
