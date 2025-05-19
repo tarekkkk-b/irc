de5391948f5d87e77ae1624c858915990ca3616a
Maha Faisal <mafaisal@lab1r2s14.42abudhabi.ae>
2025-05-19 12:04:13 +0400

    Server class stores channel and clients, and have message type defined

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -1,28 +1,35 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
- #include <sys/socket.h>
- #include <netinet/in.h>
- #include <sys/event.h>
- #include <iostream>
- #include <arpa/inet.h>
- #include <vector>
- #include "Client.hpp"
- #include <unistd.h>
- #include <unistd.h> 
-#include <string>     
-#include <iostream> 
- #define MAX_EVENTS 1024
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <sys/event.h>
+# include <iostream>
+# include <arpa/inet.h>
+# include <vector>
+# include "Client.hpp"
+# include <unistd.h>
+# include <unistd.h> 
+# include <string>     
+# include <iostream>
+# include <utility>
+# include "Channel.hpp"
+# include <map>
+
+#define MAX_EVENTS 1024
 
 class Server
 {
     private:
+        typedef std::pair<std::string, std::vector <Client const *> > message;
         int _servPort;
         std::string _servPass;
         uintptr_t _servFd;
-        // std:: vector <Client> clients_list;
+        std::map <std::string, Client const * > clients_list;
+        std:: map <std::string, Channel const * > _channels;
         int kq;
         std::vector <int> clientsSocket;
         int clientcount;
+        message parseChannelCommand(std::vector<std::string> command, Client const & sender);
         
         Server();
 
@@ -35,7 +42,8 @@ class Server
         void initServerSocket();
         void registerEvents(int fd , int16_t  filter);
         void handleEvents();
-       void registerChannelCients(std::vector <int> channelClients);
+        void registerChannelCients(std::vector <int> channelClients);
+
 
 };
 
