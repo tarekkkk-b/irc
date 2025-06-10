67c9d5c86a447bbc367a3041696e4427ee46f57a
tarekkkk-b <tarekbadawi2004@gmail.com>
2025-06-10 09:57:38 +0400

    command responses syntax was fixed

--- srcs/commands/invite.cpp
+++ srcs/commands/invite.cpp
@@ -2,21 +2,21 @@
 
 std::vector <Client * > Server::handleInvite(std::vector<std::string> command, Client & sender)
 {	
-	std::string notEnoughParams = "461: " + sender.getNick() + " " + command[0] + " :Not enough parameters\n";
-	std::string noSuchNick = "401: " + sender.getNick() + " " + command[0] + " :No such nick\n";
-	std::string noSuchChannel = "403: " + sender.getNick() + " " + command[0] + " :No such channel\n";
-	std::string noSuchNickName = "406: " + sender.getNick() + " " + command[0] + " :There was no such nickname\n";
+	std::string notEnoughParams = ":ircserver 461 " + sender.getNick() + " INVITE :Not enough parameters\r\n";
 	if (command.size() != 2)
-	return setClientsBuffer(std::vector< Client*>(1, &sender), notEnoughParams);
+		return setClientsBuffer(std::vector< Client*>(1, &sender), notEnoughParams);
+	std::string noSuchNick = ":ircserver 401 " + sender.getNick() + " " + command[0] + " :No such nick/channel\r\n";
+	std::string noSuchChannel = ":ircserver 403 " + sender.getNick() + " " + command[0] + " :No such channel\r\n";
+	std::string noSuchNickName = ":ircserver 406 " + sender.getNick() + " " + command[0] + " :There was no such nickname\r\n";
 	if (!getClientByNick(command[0]))
-	return setClientsBuffer(std::vector< Client*>(1, &sender), noSuchNickName);
+		return setClientsBuffer(std::vector< Client*>(1, &sender), noSuchNickName);
 	if (command[1][0] == '#')
 	{
 		Channel *channel = getChannel(command[1]);
 		if (channel == NULL)
-		return setClientsBuffer(std::vector< Client*>(1, &sender), noSuchChannel);
+			return setClientsBuffer(std::vector< Client*>(1, &sender), noSuchChannel);
 		else
-		return channel->inviteClient(&sender, getClientByNick(command[0]));
+			return channel->inviteClient(&sender, getClientByNick(command[0]));
 	}
 	return setClientsBuffer(std::vector< Client*>(1, &sender), noSuchNick);
 }
--- srcs/commands/topic.cpp
+++ srcs/commands/topic.cpp
@@ -3,12 +3,12 @@
 std::vector <Client * > Server::handleTopic(std::vector<std::string> command, Client & sender)
 {
 	
+	std::string notEnoughParams = ":ircserver 461 " + sender.getNick() + " TOPIC :Not enough parameters\r\n";
 	if (command.size() == 0)
-		return setClientsBuffer(std::vector< Client*>(1, &sender), "Not enough parameters\n");
-	std::string notEnoughParams = "461: " + sender.getNick() + " " + command[0] + " :Not enough parameters\n";
-	std::string noSuchNick = "401: " + sender.getNick() + " " + command[0] + " :No such nick\n";
-	std::string noSuchChannel = "403: " + sender.getNick() + " " + command[0] + " :No such channel\n";
-	std::string noSuchNickName = "406: " + sender.getNick() + " " + command[0] + " :There was no such nickname\n";
+		return setClientsBuffer(std::vector< Client*>(1, &sender), notEnoughParams);
+	std::string noSuchNick = ":ircserver 401 " + sender.getNick() + " " + command[0] + " :No such nick/channel\r\n";
+	std::string noSuchChannel = ":ircserver 403 " + sender.getNick() + " " + command[0] + " :No such channel\r\n";
+	std::string noSuchNickName = ":ircserver 406 " + sender.getNick() + " " + command[0] + " :There was no such nickname\r\n";
 	Channel * channel = NULL;
 	if (command[0][0] == '#')
 	{
